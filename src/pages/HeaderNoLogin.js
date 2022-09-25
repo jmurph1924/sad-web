905e7bcc32ac55bae2a2b8ed85bf7a340869ca8c
Jonathan Murphree <jtmurphree@bellsouth.net>
2022-09-25 18:51:13 -0400

    spacing

--- src/pages/HeaderNoLogin.js
+++ src/pages/HeaderNoLogin.js
@@ -4,22 +4,22 @@ import { Menu, Row, Col} from 'antd'
 const HeaderNoLogin = () => {        
         
     return (
-       <>
-            <Row >
-              <Col span={4}>
-                <img src={require('../images/instacount.png')} style={{width: "274px", paddingTop: "5px"}} alt="Instacount" className="logo"/>
-              </Col>
-              <Col span={20}>
-                <Menu
-                  theme="white"
-                  mode="horizontal"
-                  defaultSelectedKeys={['login']}
-                  selectedKeys={[]}
-                >
-                </Menu>
-              </Col>
-            </Row>
-    </>
+      <>
+        <Row>
+          <Col span={4}>
+            <img src={require('../images/instacount.png')} style={{width: "274px", paddingTop: "5px"}} alt="Instacount" className="logo"/>
+          </Col>
+          <Col span={20}>
+            <Menu
+              theme="white"
+              mode="horizontal"
+              defaultSelectedKeys={['login']}
+              selectedKeys={[]}
+            >
+            </Menu>
+          </Col>
+        </Row>
+      </>
     )
 }
 
