13fe427982e9516dc6f9509716b57596c7f7dc33
Cliffherr312 <113385169+Cliffherr312@users.noreply.github.com>
2022-10-17 14:58:57 -0400

    Layout
    Layout for Chart

--- src/components/ChartsOfAccounts/ChartsOfAccounts.js
+++ src/components/ChartsOfAccounts/ChartsOfAccounts.js
@@ -2,6 +2,173 @@ import * as React from "react";
 
 const ChartsAccountpage = () => {
 
+    const columns = [
+        {
+          title: 'Account Name',
+          key: 'Account Name',
+          render: item => {
+            return (
+                <>
+         
+                </>
+            )
+          }
+        },
+        {
+          title: 'Account Number',
+          key: 'Account Number',
+          render: item => {
+            return (
+                <>
+      
+                </>
+            )
+          }
+        },
+        {
+          title: 'Account Description',
+          key: 'Account Description',
+          render: item => {
+            return (
+            <>
+              
+            </>
+            )
+          }
+        },
+        {
+          title: 'Normal Side',
+          key: 'Normal Side',
+          render: item => {
+            return (
+            <>
+              
+            </>
+            )
+          }
+        },
+        {
+          title: 'Account Category',
+          key: 'Account Category',
+          render: item => {
+            return (
+              <>
+                
+              </>
+            )
+          }
+        },
+        {
+          title: 'Account Subcategory',
+          key: 'Account Subcategory',
+          render: item => {
+            return (
+              <>
+             
+              </>
+            )
+          }
+        },
+        {
+            title: 'Initial Balance',
+            key: 'Initial Balance',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Debit',
+            key: 'Debit',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Credit',
+            key: 'Credit',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Balance',
+            key: 'Balance',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Date/Time Account Added',
+            key: 'Date/Time Account Added',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'User id',
+            key: 'User id',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Order',
+            key: 'Order',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Statement',
+            key: 'Statement',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        },
+        {
+            title: 'Comment',
+            key: 'Comment',
+            render: item => {
+              return (
+                <>
+               
+                </>
+              )
+            }
+        }
+    ]
     return(
         <h1>Charts Of Accounts | From Components</h1>
     );
